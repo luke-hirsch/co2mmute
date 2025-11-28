ff849cfb7d74eebbd787150d7b774fc62776119e
Sebastian Werblinski <sebastian.werblinski@gmail.com>
2025-11-28 11:57:03 +0100

    models.py integrated models

--- backend/maps/models.py
+++ backend/maps/models.py
@@ -1,10 +1,155 @@
+from xml.dom import ValidationErr
 from django.db import models
 
 
 # Create your models here.
 class GameMap(models.Model):
+
     map_name = models.CharField(max_length=100)
+    map_file = models.FileField(upload_to='maps/')
     description = models.TextField()
+    x_dim = models.PositiveIntegerField() 
+    y_dim = models.PositiveIntegerField() 
+    scale = models.FloatField(help_text="Distance between grid center points in km") # scale in km
+    train_speed = models.FloatField(help_text="Default train speed limit in km/h") 
+    bike_speed = models.FloatField(help_text="Default bike speed in km/h")
+    pedestrian_speed = models.FloatField(help_text="Default pedestrian speed in km/h")
+    max_street_lanes = models.PositiveIntegerField(help_text="Maximum number of street lanes including bus lane") 
+    max_bus_capacity = models.PositiveIntegerField(help_text="Maximum capacity of a bus line")   
+    max_train_capacity = models.PositiveIntegerField(help_text="Maximum capacity of a train line")
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True) 
 
     def __str__(self):
-        return self.map_name
+        return f"Map: {self.map_name} with xy-dimensions: ({self.x_dim}x{self.y_dim} at {self.scale} km scale)"
+
+    def load_map_file(self):
+        # method to load and parse the map file if needed
+        pass
+
+class PTLine(models.Model):
+    
+    game_map = models.ForeignKey(GameMap, on_delete=models.CASCADE, related_name='related_map')
+    frequency = models.PositiveIntegerField(help_text="Frequency of the line in minutes")
+    line_number = models.PositiveIntegerField(max_length=10)
+    capacity = models.PositiveIntegerField(help_text="Maximum capacity of the line")
+
+    def __str__(self):
+        return f"PT Line: {self.line_number} of type on map {self.game_map}"
+    
+    class Meta:
+        abtract = True
+
+class BusLine(PTLine):
+
+    def save(self, *args, **kwargs):
+        if self.capacity > self.game_map.max_bus_capacity:
+            raise ValidationErr(f"Bus line capacity {self.capacity} exceeds maximum bus capacity {self.game_map.max_bus_capacity} for map {self.game_map}.")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Bus Line: {self.line_number} on map {self.game_map}"  
+
+class TrainLine(PTLine):
+
+    def save(self, *args, **kwargs):
+        if self.capacity > self.game_map.max_train_capacity:
+            raise ValidationErr(f"Train line capacity {self.capacity} exceeds maximum train capacity {self.game_map.max_train_capacity} for map {self.game_map}.")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Train Line: {self.line_number} on map {self.game_map}"
+
+class Node(models.Model):
+
+    game_map = models.ForeignKey(GameMap, on_delete=models.CASCADE, related_name='related_map')
+    x_coord = models.FloatField()
+    y_coord = models.FloatField()
+    nodetype_choices = [
+        ('H', 'Household'), 
+        ('W', 'Workplace'),
+        ('TS', 'Train Station'),
+        ('I', 'Intersection'),
+        ('BS', 'Bus Stop'),
+    ]
+    node_name = models.CharField(max_length=100, null=True, blank=True)
+
+    # methods for node
+    def clean(self):
+        if self.x_coord < 0 or self.x_coord > self.game_map.x_dim:
+            raise ValidationErr(f"x_coord {self.x_coord} is out of bounds for map dimension {self.game_map.x_dim}")
+        if self.y_coord < 0 or self.y_coord > self.game_map.y_dim:
+            raise ValidationErr(f"y_coord {self.y_coord} is out of bounds for map dimension {self.game_map.y_dim}")   
+
+    def save(self, *args, **kwargs):
+        self.full_clean()  # call clean and validate the model fields
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Node {self.id} belonging to {self.game_map}. Node is of type {self.nodetype} located at ({self.x_coord}, {self.y_coord})."
+
+
+class Edge(models.Model):
+
+    game_map = models.ForeignKey(GameMap, on_delete=models.CASCADE, related_name='related_map')
+    node1 = models.ForeignKey(Node, on_delete=models.CASCADE, related_name='from_node')
+    node2 = models.ForeignKey(Node, on_delete=models.CASCADE, related_name='to_node')
+    edgetype_choices = [
+        ('S', 'Street'),
+        ('BK', 'Bike Path'),
+        ('T', 'Train Track'),
+        ('PW', 'Pedestrian Walkway'),
+    ]
+    distance = models.FloatField(help_text="Distance of the edge in km", editable=False) 
+    edge_name = models.CharField(max_length=100, null=True, blank=True) # for map visualization
+
+
+    def clean(self):
+        if self.node1.game_map != self.game_map or self.node2.game_map != self.game_map:
+            raise ValidationErr("Start and end nodes must belong to the same game map as the edge.")
+
+    def save(self, *args, **kwargs):
+        # prevent creating plain edge without subtype
+        if self.__class__ == Edge:
+            raise ValidationErr("Cannot create an Edge instance directly. Please use a specific Edge subtype.")
+
+        self.full_clean()  # call clean and validate the model fields
+
+        # calculate distance based on node coordinates and map scale
+        dx = self.node1.x_coord - self.to_node.x_coord
+        dy = self.node1.y_coord - self.to_node.y_coord
+        self.distance = (dx*dx + dy*dy) ** 0.5 * self.game_map.scale
+        
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Edge belonging to {self.game_map}. Edge is of type {self.edgetype_choices} running from {self.node1} to {self.to_node}"
+    
+
+class StreetEdge(Edge):
+    speed_limit = models.FloatField(help_text="Speed limit in km/h")
+    number_of_lanes = models.PositiveIntegerField(help_text="Number of lanes (including bus lane)")
+    bus_lane = models.BooleanField(default=False, help_text="Indicates if there is a bus lane")
+    bus_lines = models.ManyToManyField(BusLine, blank=True)
+
+    def __str__(self):
+        return f"Street Edge from {self.node1} to {self.node2} with speed limit {self.speed_limit} km/h"
+    
+class BikePathEdge(Edge):
+    bike_lane_type = models.CharField(max_length=100, help_text="Type of bike lane")
+
+    def __str__(self):
+        return f"Bike Path Edge from {self.node1} to {self.node2} of type {self.bike_lane_type}"
+    
+class TrainTrackEdge(Edge):
+    train_lines = models.ManyToManyField(TrainLine, blank=True)
+
+    def __str__(self):
+        return f"Train Track Edge from {self.node1} to {self.node2} with {self.number_of_tracks} tracks"
+    
+class PedestrianWalkwayEdge(Edge):
+
+    def __str__(self):
+        return f"Pedestrian Walkway Edge from {self.node1} to {self.node2}"
+
+
