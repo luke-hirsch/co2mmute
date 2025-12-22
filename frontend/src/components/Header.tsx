eb9c7f5de7f73ee7ec367c880356123d0f3d162d
Lukas <lukas@luke-hirsch.de>
2025-12-22 21:35:27 +0100

    favicons

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,3 @@
-import logo from "../assets/logo.svg";
-
 interface HeaderProps {
   title: string;
   icon?: string;
@@ -22,7 +20,7 @@ export default function Header({
         target="_blank"
         className="transition-opacity duration-200 hover:opacity-80"
       >
-        <img src={logo} className="-translate-y-3 h-16 w-auto" alt="Logo" />
+        <img src="/logo.svg" className="-translate-y-3 h-16 w-auto" alt="Logo" />
       </a>
       <h2 className=" text-xl font-light text-muted transition-colors duration-300 md:text-2xl dark:text-darkmutedtext">
         {title}
