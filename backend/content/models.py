0299cf800d8b3e8ea7b192054c412fb25d6c22c9
Sebastian Werblinski <sebastian.werblinski@gmail.com>
2026-02-01 21:27:11 +0100

    Added alignment to columns and static template

--- backend/content/models.py
+++ backend/content/models.py
@@ -85,6 +85,11 @@ class ContentBlockColumn(models.Model):
         default=33, 
         help_text="Width of column in percent (1-100)",
         validators=[MinValueValidator(1), MaxValueValidator(100)],)
+    alignment = models.Choices(
+        LEFT = "left", "Left",
+        CENTER = "center", "Center",
+        RIGHT = "right", "Right",
+    )
     
     # optional fields if kind is text
     title = models.CharField(max_length=200, blank=True)
